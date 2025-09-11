70bb78d94095feb18ee204ffa4070dbbdce8a9b1
Oliur Rahman <62242483+oliur-portfolio@users.noreply.github.com>
2025-09-11 15:49:51 +0600

    home page

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("JS code");
+// Trust Review Slider
+var swiperChallange = new Swiper(".swiper--trust", {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination--trust",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next--trust",
+    prevEl: ".swiper-button-prev--trust",
+  },
+});
+
+// HAMBURGER MENU TOGGLER
+document.querySelectorAll(".navbar__toggler").forEach((toggler) => {
+  toggler.addEventListener("click", () => {
+    const headerArea = toggler.closest(".header");
+    if (headerArea) {
+      headerArea.classList.toggle("header--show");
+    }
+    toggler.classList.toggle("change");
+    document.body.classList.toggle("overflow-hidden");
+  });
 });
